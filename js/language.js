31af764c694c72c64f5ff0197ed2ffceb6049395
Ivan Cristina <ivancristina5@gmail.com>
2026-02-07 18:37:52 +0100

    Loading Pics

---
+++ js/language.js
@@ -0,0 +1,10 @@
+let language;
+
+switch (language) {
+  case "english":
+    console.log(`Language is italian ${expr}.`);
+  case "german":
+    console.log(`Language is italian ${expr}.`);
+  default:
+    console.log(`Language is italian ${expr}.`);
+}
